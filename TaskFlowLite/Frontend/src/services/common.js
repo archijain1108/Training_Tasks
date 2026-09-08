3145402a939726b94eeebeaacb4cbbeb53ca7dbc
archijain1108 <arcjain@trueigtech.com>
2026-09-08 10:59:49 +0530

    fix : configure backend url

--- TaskFlowLite/Frontend/src/services/common.js
+++ TaskFlowLite/Frontend/src/services/common.js
@@ -1,9 +1,6 @@
 import axios from 'axios'
-console.log(import.meta.env.VITE_BACKEND_URL)
 
 export const api = axios.create({
-    baseURL : import.meta.env.VITE_BACKEND_URL,
+    baseURL : import.meta.env.VITE_BACKEND_URL || 'http://localhost:8080/api',
     withCredentials : true
 })
-
-
